a982e0563b621b07c306faa9d58a6cb760bf2386
Larry <larry@Lawrences-iMac.local>
2021-02-18 10:40:43 +0000

    first commit

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+
+function header() {
+    return (
+        <div className='header'>
+             <div className="header__left">
+                <img 
+                src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg" 
+                alt=""
+                />
+                
+                <div className="header__search">
+                  <SearchIcon />
+                  <input type="text" />
+                </div>
+             </div>
+
+             <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home"/>
+                <HeaderOption Icon={SupervisorAccountIcon}title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notification"/>
+                <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C4D03AQHbcL0-zPl7iQ/profile-displayphoto-shrink_100_100/0/1538855962920?e=1619049600&v=beta&t=dNyvc5gniFdGkS-ATNQ7BfFI1ReaQqbNpS5yc9Gyk0Q" title="Larry"/>
+             </div>
+        </div>
+    )
+}
+
+export default header
